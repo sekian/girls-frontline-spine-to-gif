f18bd1e4a62d2c09f0d62b2b97d6037f3dfe7612
Sekian <skillout@hotmail.es>
2019-04-09 02:38:13 +0200

    Update CreateChibiFrames.java

--- src/CreateChibiFrames.java
+++ src/CreateChibiFrames.java
@@ -47,9 +47,9 @@ public class CreateChibiFrames extends ApplicationAdapter {
     public void create () {
         long start2 = System.currentTimeMillis();
         int nThreads = Runtime.getRuntime().availableProcessors();
-    	pool = new ThreadPoolExecutor(2, 2, 10, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
-		batch = new PolygonSpriteBatch();
-		renderer = new SkeletonRenderer();
+        pool = new ThreadPoolExecutor(2, 2, 10, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+        batch = new PolygonSpriteBatch();
+        renderer = new SkeletonRenderer();
         renderer.setPremultipliedAlpha(true);
         FileHandle dirHandle = Gdx.files.absolute("./");        
         FileHandle[] dir1 = dirHandle.list(".skel");
@@ -57,8 +57,8 @@ public class CreateChibiFrames extends ApplicationAdapter {
         FileHandle[] dir = new FileHandle[dir1.length + dir2.length];
         System.arraycopy(dir1, 0, dir, 0, dir1.length);
         System.arraycopy(dir2, 0, dir, dir1.length, dir2.length);
-		File file = new File("_out");
-		file.mkdirs();
+        File file = new File("_out");
+        file.mkdirs();
         for  (FileHandle skeletonFile: dir) {
             long start = System.currentTimeMillis();
             SkeletonData skeletonData = loadSkeleton(skeletonFile);
@@ -66,29 +66,29 @@ public class CreateChibiFrames extends ApplicationAdapter {
             if (skeletonName.endsWith(".skel")) skeletonName = skeletonName.substring(0, skeletonName.length() - 5);
             System.out.print(skeletonName+" ");            
             FileOutputStream fout = null;
-			try {
-				fout = new FileOutputStream("_out/" + skeletonName + ".zip");
-			} catch (FileNotFoundException e1) {e1.printStackTrace();}
-    		BufferedOutputStream bout = new BufferedOutputStream(fout);
-    		ZipOutputStream zout = new ZipOutputStream(bout);  
+            try {
+                fout = new FileOutputStream("_out/" + skeletonName + ".zip");
+            } catch (FileNotFoundException e1) {e1.printStackTrace();}
+            BufferedOutputStream bout = new BufferedOutputStream(fout);
+            ZipOutputStream zout = new ZipOutputStream(bout);  
             for (Animation animation : skeletonData.getAnimations()) {
                 Vector2 min = new Vector2(0,0);
                 Vector2 max = new Vector2(0,0);
                 Vector2 pos = new Vector2(0,0);
                 getAnimationBounds(min, max, skeletonData, animation.getName());
-				pos.x = Math.abs(min.x); 
-				pos.y = Math.abs(min.y); 
-				max.x += Math.abs(min.x);
-				max.y += Math.abs(min.y);
-				min.x = 0;
-				min.y = 0;
-            	try {saveAnimation(min, max, pos, skeletonData, animation.getName(), zout);} 
-            	catch (Exception e) {e.printStackTrace();}      
+                pos.x = Math.abs(min.x); 
+                pos.y = Math.abs(min.y); 
+                max.x += Math.abs(min.x);
+                max.y += Math.abs(min.y);
+                min.x = 0;
+                min.y = 0;
+                try {saveAnimation(min, max, pos, skeletonData, animation.getName(), zout);} 
+                catch (Exception e) {e.printStackTrace();}      
             }
             try {zout.close();
-        		bout.close();
-        		fout.close();} 
-	        catch (IOException e) { e.printStackTrace(); }            
+                bout.close();
+                fout.close();} 
+            catch (IOException e) { e.printStackTrace(); }            
             long end = System.currentTimeMillis();
             System.out.print((end - start)/1000.0 + "s\n");
         }
@@ -100,30 +100,30 @@ public class CreateChibiFrames extends ApplicationAdapter {
         if (skeletonFile == null) return null;
         String atlasFileName = skeletonFile.nameWithoutExtension();
         if (atlasFileName.endsWith(".skel"))
-        	atlasFileName = atlasFileName.substring(0, atlasFileName.length() - 5);
+            atlasFileName = atlasFileName.substring(0, atlasFileName.length() - 5);
         FileHandle atlasFile = skeletonFile.sibling(atlasFileName + ".atlas");
         if (!atlasFile.exists()) 
-        	atlasFile = skeletonFile.sibling(atlasFileName + ".atlas.txt");        
+            atlasFile = skeletonFile.sibling(atlasFileName + ".atlas.txt");        
         if (!atlasFile.exists() && atlasFileName.charAt(0) == 'R')
-			atlasFile = skeletonFile.sibling(atlasFileName.substring(1) + ".atlas");			
+            atlasFile = skeletonFile.sibling(atlasFileName.substring(1) + ".atlas");            
         if (!atlasFile.exists() && atlasFileName.charAt(0) == 'R') 
-			atlasFile = skeletonFile.sibling(atlasFileName.substring(1) + ".atlas.txt");		
+            atlasFile = skeletonFile.sibling(atlasFileName.substring(1) + ".atlas.txt");        
         if (!atlasFile.exists() && atlasFileName.charAt(0) == 'R')
-			atlasFile = skeletonFile.sibling(atlasFileName.substring(1) + ".atlas");	
+            atlasFile = skeletonFile.sibling(atlasFileName.substring(1) + ".atlas");    
         if (!atlasFile.exists() && atlasFileName.charAt(0) == 'R') 
-			atlasFile = skeletonFile.sibling(atlasFileName.substring(1) + ".atlas.txt");
-        if (!atlasFile.exists() && atlasFileName.endsWith("_NPC"))      	
-        	atlasFile = skeletonFile.sibling(atlasFileName.substring(0, atlasFileName.length() - 3)+ "Boss" + ".atlas");
+            atlasFile = skeletonFile.sibling(atlasFileName.substring(1) + ".atlas.txt");
+        if (!atlasFile.exists() && atlasFileName.endsWith("_NPC"))          
+            atlasFile = skeletonFile.sibling(atlasFileName.substring(0, atlasFileName.length() - 3)+ "Boss" + ".atlas");
         if (!atlasFile.exists() && atlasFileName.endsWith("_NPC"))
-			atlasFile = skeletonFile.sibling(atlasFileName.substring(0, atlasFileName.length() - 3)+ "Boss" + ".atlas.txt");
+            atlasFile = skeletonFile.sibling(atlasFileName.substring(0, atlasFileName.length() - 3)+ "Boss" + ".atlas.txt");
         if (!atlasFile.exists() && atlasFileName.contains("_"))
-        	atlasFile = skeletonFile.sibling(atlasFileName.substring(0, atlasFileName.lastIndexOf('_'))+ ".atlas");
+            atlasFile = skeletonFile.sibling(atlasFileName.substring(0, atlasFileName.lastIndexOf('_'))+ ".atlas");
         if (!atlasFile.exists() && atlasFileName.contains("_"))
-			atlasFile = skeletonFile.sibling(atlasFileName.substring(0, atlasFileName.lastIndexOf('_'))+ ".atlas.txt");
+            atlasFile = skeletonFile.sibling(atlasFileName.substring(0, atlasFileName.lastIndexOf('_'))+ ".atlas.txt");
         if (!atlasFile.exists() && (atlasFileName.endsWith("A") || atlasFileName.endsWith("B") || atlasFileName.endsWith("C")))
-        	atlasFile = skeletonFile.sibling(atlasFileName.substring(1, atlasFileName.length() - 1)+ ".atlas");
+            atlasFile = skeletonFile.sibling(atlasFileName.substring(1, atlasFileName.length() - 1)+ ".atlas");
         if (!atlasFile.exists() && (atlasFileName.endsWith("A") || atlasFileName.endsWith("B") || atlasFileName.endsWith("C")))
-			atlasFile = skeletonFile.sibling(atlasFileName.substring(1, atlasFileName.length() - 1)+ ".atlas.txt");
+            atlasFile = skeletonFile.sibling(atlasFileName.substring(1, atlasFileName.length() - 1)+ ".atlas.txt");
         
         TextureAtlasData data = new TextureAtlasData(atlasFile, atlasFile.parent(), false);
         TextureAtlas atlas = new TextureAtlas(data);        
@@ -216,7 +216,7 @@ public class CreateChibiFrames extends ApplicationAdapter {
         //System.out.println(maxX +" "+ maxY);
     }
     public void saveAnimation (Vector2 min, Vector2 max, Vector2 pos, SkeletonData skeletonData, String animation, ZipOutputStream zout) throws Exception {
-    	AnimationState state = new AnimationState(new AnimationStateData(skeletonData));
+        AnimationState state = new AnimationState(new AnimationStateData(skeletonData));
         renderer.setPremultipliedAlpha(true);
         Skeleton skeleton = new Skeleton(skeletonData);
         skeleton.setToSetupPose();
@@ -241,22 +241,22 @@ public class CreateChibiFrames extends ApplicationAdapter {
             int maxX = (int)Math.ceil(max.x)-minX;
             int maxY = (int)Math.ceil(max.y)-minY;
             byte[] pixels = ScreenUtils.getFrameBufferPixels(minX, minY, maxX, maxY, true);
-    		BufferedImage image = new BufferedImage(maxX, maxY, BufferedImage.TYPE_INT_ARGB);    
-    		for (int x = 0; x < maxX; x++) {
-    		    for (int y = 0; y < maxY; y++) {
-    		        int i = (x + (maxX * y)) * 4;
-    		        int r = pixels[i + 0] & 0xFF;
-    		        int g = pixels[i + 1] & 0xFF;
-    		        int b = pixels[i + 2] & 0xFF;
-    		        int a = pixels[i + 3] & 0xFF;
-    		        image.setRGB(x, y, (a << 24) | (r << 16) | (g << 8) | b);
-    		    }
-    		}
-    		String filePath = String.format(state.getCurrent(0)+"/%04d.gif", numFrame);
-	        ZipEntry entry = new ZipEntry(filePath);
-			zout.putNextEntry(entry);
-	        ImageIO.write(image, "gif", zout);
-	        zout.closeEntry();
+            BufferedImage image = new BufferedImage(maxX, maxY, BufferedImage.TYPE_INT_ARGB);    
+            for (int x = 0; x < maxX; x++) {
+                for (int y = 0; y < maxY; y++) {
+                    int i = (x + (maxX * y)) * 4;
+                    int r = pixels[i + 0] & 0xFF;
+                    int g = pixels[i + 1] & 0xFF;
+                    int b = pixels[i + 2] & 0xFF;
+                    int a = pixels[i + 3] & 0xFF;
+                    image.setRGB(x, y, (a << 24) | (r << 16) | (g << 8) | b);
+                }
+            }
+            String filePath = String.format(state.getCurrent(0)+"/%04d.gif", numFrame);
+            ZipEntry entry = new ZipEntry(filePath);
+            zout.putNextEntry(entry);
+            ImageIO.write(image, "gif", zout);
+            zout.closeEntry();
             ++numFrame;
         }
     }
